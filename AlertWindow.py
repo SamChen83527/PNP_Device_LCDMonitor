6ffc90342f62839f550aed9d9d52be6519f6a153
SamChen83527 <s83527@g.ncu.edu.tw>
2019-11-06 16:06:20 +0800

    Adjust font size

--- AlertWindow.py
+++ AlertWindow.py
@@ -12,7 +12,7 @@ window = tk.Tk()
 window.title('Fire Alert')
 
 # 第3步，設定視窗的大小(長 * 寬)
-window.geometry('1000x600')  # 這裡的乘是小x
+window.geometry('2000x1200')  # 這裡的乘是小x
 
 # 第4步，定義觸發函式功能
 def show_alert ():
@@ -21,33 +21,45 @@ def show_alert ():
         msg = ''
         # read msg from XBee
         # task template- <DeviceID>$<msg>
-        while True:
-            msg = SerialPortManager().readMsg()
-            if msg != '':
-                msg_split = msg.strip().split('$')
-                deviceID = msg_split[0]
-                text = msg_split[1]
-                print (deviceID)
-                print (text)
-                
-                case1 = 'Right-Stairs-2-1'
-                case2 = 'Level-2-Mid-Corridor-E'
-                case3 = 'Level-2-Right-Corridor-B'
-                
-                if deviceID == Device_ID:
-                    if text == case1:
-                        print ('case 1')
-                        tkinter.messagebox.showwarning(title='Warning', message='Warning:\n Please go downstairs for evacuation!')       # 提出警告對話窗
-                        break
-                    elif text == case2:
-                        print ('case 2')
-                        tkinter.messagebox.showwarning(title='Warning', message='Warning: Please turn right for evacuation!')       # 提出警告對話窗
-                        break
-                    elif text == case3:
-                        print ('case 3')
-                        tkinter.messagebox.showwarning(title='Warning', message='Warning: Please trun left for evacuation!')       # 提出警告對話窗
-                        break
+        
+        try:
+            while True:
+                msg = SerialPortManager().readMsg()
+                if msg != '':
+                    msg_split = msg.strip().split('$')
+                    deviceID = msg_split[0]
+                    text = msg_split[1]
+                    print (deviceID)
+                    print (text)
+                    
+                    case1 = 'Right-Stairs-2-1'
+                    case2 = 'Level-2-Mid-Corridor-E'
+                    case3 = 'Level-2-Right-Corridor-B'
+                    
+                    if deviceID == Device_ID:
+                        if text == case1:
+                            print ('case 1')
+                            tkinter.messagebox.showwarning(title='Warning', message='Warning:\n Please go downstairs for evacuation!')       # 提出警告對話窗
+                            break
+                        elif text == case2:
+                            print ('case 2')
+                            tkinter.messagebox.showwarning(title='Warning', message='Warning: Please turn right for evacuation!')       # 提出警告對話窗
+                            break
+                        elif text == case3:
+                            print ('case 3')
+                            tkinter.messagebox.showwarning(title='Warning', message='Warning: Please trun left for evacuation!')       # 提出警告對話窗
+                            break
+        except Exception as e:
+            error_class = e.__class__.__name__ #取得錯誤類型
+            detail = e.args[0] #取得詳細內容
+            cl, exc, tb = sys.exc_info() #取得Call Stack
+            lastCallStack = traceback.extract_tb(tb)[-1] #取得Call Stack的最後一筆資料
+            fileName = lastCallStack[0] #取得發生的檔案名稱
+            lineNum = lastCallStack[1] #取得發生的行號
+            funcName = lastCallStack[2] #取得發生的函數名稱
+            errMsg = "File \"{}\", line {}, in {}: [{}] {}".format(fileName, lineNum, funcName, error_class, detail)
+            print(errMsg)
 
-tk.Button(window, text='開始火災警戒', bg='white', font=('Arial', 18), command=show_alert).pack()
+tk.Button(window, text='開始火災警戒', bg='white', font=('Arial', 128), command=show_alert).pack()
 
 window.mainloop()
